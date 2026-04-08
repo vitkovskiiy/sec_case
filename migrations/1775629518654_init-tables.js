8787bf056c9cc9ee968f88425653a467b6b5d190
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-08 09:38:23 +0300

    configure that db now has migration before every container up

---
+++ migrations/1775629518654_init-tables.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('repositories', {
+    name: { type: 'varchar(255)', primaryKey: true },
+    last_seen_tag: { type: 'varchar(255)', notNull: false },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.createTable('subscriptions', {
+    id: 'id',
+    email: { type: 'varchar(255)', notNull: true },
+    repo_name: {
+      type: 'varchar(255)',
+      notNull: true,
+      references: '"repositories"',
+      onDelete: 'CASCADE',
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.createIndex('subscriptions', ['email', 'repo_name'], { unique: true });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('subscriptions');
+  pgm.dropTable('repositories');
+};
+
