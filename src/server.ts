47c2c8690851dbe624a20134a3cab5c84eb5e58a
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-10 07:45:02 +0300

    init

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
+import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Work' });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
