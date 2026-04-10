bea0fa660278127b6119ab04e4d4efdb5b32d1d0
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-11 02:59:44 +0300

    configure ci pipeline

--- src/test/unit/sendMail.test.ts
+++ src/test/unit/sendMail.test.ts
@@ -3,7 +3,7 @@ import { Mailer } from '../../infrastructure/mailer';
 
 test('If miler working correctly return true', async () => {
     const token:string = "1s2f3f"
-    const FAKE_CLIENT = 'vitkovskyi.i.s.-im44@edu.kpi.ua';
+    const FAKE_CLIENT = 'test@gmail.com';
     const mailer = new Mailer()
     const result = await mailer.sendMail(FAKE_CLIENT,token);
     expect(result).toHaveProperty('messageId');
--- src/test/unit/token.repository.test.ts
+++ src/test/unit/token.repository.test.ts
@@ -1,13 +1,36 @@
-import { test, expect} from 'vitest';
-import { tokenRepository } from '../..';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { TokenRepository } from '../../infrastructure/repository/token.repository';
+import { Pool } from 'pg';
 
+describe('TokenRepository (Unit Test with Mocks)', () => {
+  let repository: TokenRepository;
+  let mockPool: any;
 
-test('Validate token', async () => {
-    const token:string = "6828512"
-    const fakeToken:string = "6828512"
-    const FAKE_CLIENT = 'vitkovskyi.i.s.-im44@edu.kpi.ua';
-    const failToken = await tokenRepository.validateToken(fakeToken);
-    const comapreToken = await tokenRepository.validateToken(token);
-    console.log(comapreToken);
-    expect(comapreToken).returned(true);
+  beforeEach(() => {
+    mockPool = {
+      query: vi.fn()
+    };
+
+    repository = new TokenRepository(mockPool as unknown as Pool);
+  });
+
+  it('should return true if token is valid and update is successful', async () => {
+    mockPool.query.mockResolvedValueOnce({
+      rowCount: 1,
+      rows: [{ email: 'flicks2006@gmail.com', is_confirmed: false }]
+    });
+    mockPool.query.mockResolvedValueOnce({
+      rowCount: 1,
+      rows: [{ id: 4 }]
+    });
+    const result = await repository.validateToken('6828512');
+    expect(result).toBe(true);
+    expect(mockPool.query).toHaveBeenCalledTimes(2);
+  });
+  it('should return false if token not found', async () => {
+    mockPool.query.mockResolvedValueOnce({ rowCount: 0, rows: [] });
+    const result = await repository.validateToken('wrong-token');
+    expect(result).toBe(false);
+    expect(mockPool.query).toHaveBeenCalledTimes(1);
+  });
 });
